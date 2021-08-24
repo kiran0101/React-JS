dd9195e2aef0bb009e1a6a41c0973c974375958b
unknown <shimpikiran0101@gmail.com>
2021-08-24 14:31:47 +0530

    change header

--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -39,7 +39,7 @@ function TodoList() {
 
     return (
         <div>
-            <h1>What's the plan?</h1>
+            <h1>What's the plan for today?</h1>
             <TodoForm onSubmit= {addTodo}/>
             <Todo todos={todos} completeTodo={completeTodo} 
             removeTodo={removeTodo} updateTodo={updateTodo}/>
